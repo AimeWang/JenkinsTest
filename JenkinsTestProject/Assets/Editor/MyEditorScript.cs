d6bf6d7e3a49e2e5a86de809ff3da9787f6f9b72
Aime <18766034290@163.com>
2019-10-29 15:47:19 +0800

    add method

---
+++ JenkinsTestProject/Assets/Editor/MyEditorScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+class MyEditorScript
+{
+    static void MyMethod()
+    {
+        string executeMethod = "MyEditorScript.MyMethod";
+        foreach (string arg in System.Environment.GetCommandLineArgs())
+        {
+            Debug.Log(arg);
+        }
+    }
+}
